528e5117b74677507d352e54aa8191712aa154b0
Dubu0312 <ducmanh031252@gmail.com>
2024-01-22 12:48:59 +0700

    manhnd

--- app/src/main/java/com/ltdd/nhakhoaapp/activity/LoginActivity.java
+++ app/src/main/java/com/ltdd/nhakhoaapp/activity/LoginActivity.java
@@ -24,6 +24,44 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText edtEmail, edtPassword;
@@ -35,11 +73,25 @@ public class LoginActivity extends AppCompatActivity {
 
     private Patient patient;
 
+    FusedLocationProviderClient mFusedLocationClient;
+
+    TextView latitudeTextView, longitTextView;
+    int PERMISSION_ID = 44;
+    private TextView weatherTextView;
+
     @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        latitudeTextView = findViewById(R.id.latTextView);
+        longitTextView = findViewById(R.id.lonTextView);
+        weatherTextView = findViewById(R.id.weatherTextView);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // method to get the location
+        getLastLocation();
 
         edtEmail = findViewById(R.id.email_dn);
         edtPassword = findViewById(R.id.pass_dn);
@@ -72,7 +124,7 @@ public class LoginActivity extends AppCompatActivity {
 
                 // Check WiFi signal strength before attempting to login
                 int signalStrength = getWiFiSignalStrength();
-                tvSignalStrength.setText("WiFi Signal Strength: " + signalStrength + "/4");
+                tvSignalStrength.setText("Cường Độ WiFi: " + signalStrength + "/4");
 
                 if (signalStrength == 0) {
                     Toast.makeText(LoginActivity.this, "Unable to connect to the internet", Toast.LENGTH_SHORT).show();
@@ -126,5 +178,177 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(LoginActivity.this, "Call error in " + this.getClass().getSimpleName(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        // check if permissions are given
+        if (checkPermissions()) {
+
+            // check if location is enabled
+            if (isLocationEnabled()) {
+
+                // getting last
+                // location from
+                // FusedLocationClient
+                // object
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                            latitudeTextView.setText("Vĩ Tuyến: " + location.getLatitude() + "");
+                            longitTextView.setText("Kinh Tuyến: " + location.getLongitude() + "");
+
+                            new GetWeatherTask().execute(location.getLatitude(), location.getLongitude());
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            // if permissions aren't available,
+            // request for permissions
+            requestPermissions();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        // Initializing LocationRequest
+        // object with appropriate methods
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        // setting LocationRequest
+        // on FusedLocationClient
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            latitudeTextView.setText("Latitude: " + mLastLocation.getLatitude() + "");
+            longitTextView.setText("Longitude: " + mLastLocation.getLongitude() + "");
+        }
+    };
+
+    // method to check for permissions
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        // If we want background location
+        // on Android 10.0 and higher,
+        // use:
+        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // method to request for permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    // method to check
+    // if location is enabled
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // If everything is alright then
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+    }
+
+    private class GetWeatherTask extends AsyncTask<Double, Void, String> {
+
+        @Override
+        protected String doInBackground(Double... params) {
+            double latitude = params[0];
+            double longitude = params[1];
+            String apiKey = "ffe295f70f09189e5f41a89b53e34811";
+            String apiUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=metric";
+
+            try {
+                URL url = new URL(apiUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder jsonResult = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    jsonResult.append(line).append("\n");
+                }
+
+                reader.close();
+                connection.disconnect();
+                return jsonResult.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            if (result != null) {
+                try {
+                    JSONObject json = new JSONObject(result);
+
+                    // Extracting weather information
+                    JSONArray weatherArray = json.getJSONArray("weather");
+                    JSONObject weather = weatherArray.getJSONObject(0);
+                    String main = weather.getString("main");
+                    String description = weather.getString("description");
+
+                    // Extracting temperature information
+                    JSONObject mainObject = json.getJSONObject("main");
+                    double temperature = mainObject.getDouble("temp");
+                    double humidity = mainObject.getDouble("humidity");
+
+                    String weatherInfo = "Thời Tiết: " + main + "\nNhiệt Độ: " + temperature + "°C" + "\nĐộ Ẩm: " + humidity + "%";
+
+                    weatherTextView.setText(weatherInfo);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    weatherTextView.setText("Failed to fetch weather information");
+                }
+            } else {
+                weatherTextView.setText("Failed to fetch weather information");
+            }
+        }
     }
 }
